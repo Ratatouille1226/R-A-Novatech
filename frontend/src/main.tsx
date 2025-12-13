acf2e7bf8a9e8fa3292add8d5399fe33ba68c5be
Andrey <kotlyar.andrey03@mail.ru>
2025-12-13 23:58:56 +0300

    Размещаю сайт на гитхаб пейджес (чтобы затестить), поправил пусть css стилей в футере

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./i18n.ts";
 import App from "./App.tsx";
 import "./variables.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ScrollToTop } from "./components/index.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <StrictMode>
       <ScrollToTop />
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
